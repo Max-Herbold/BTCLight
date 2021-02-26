8eadfdde49e71f81bf69685e6fa66e13050dce7a
someone <someone@someplace.com>
2021-02-26 00:19:02 +0000

    Fixed timezone issue and fixed keyboard interrupt

--- main.py
+++ main.py
@@ -38,7 +38,8 @@ def backwardsWipe(strip, clearTo, prev, wait_ms=50):
             strip.show()
             time.sleep(wait_ms/1000.0)
 
-def update(strip,c,prev,colors,delay):
+def update(strip,c,prev,b,delay):
+    colors = [Color(b,0,0),Color(0,b,0)]
     backwardsWipe(strip,c,prev,delay)
     colorWipe(strip,colors[int(c>0)],abs(c),delay)
 
@@ -49,9 +50,7 @@ strip.begin()
 b = 10 # brightness (0-255)
 d = 0 # delay in ms
 
-red = Color(b,0,0)
-green = Color(0,b,0)
-colors= [red,green]
+stop = False
 
 prev = 0
 coin = btc.btc()
@@ -67,35 +66,40 @@ def quit():
 def updateB():
     global b
     while 1:
-        d = datetime.datetime.now()
-        day = d.day+(d.month-1)*30                   # gets the day of the year aprox
+        d = datetime.datetime.now()+datetime.timedelta(hours=11) # added shift to match local time
+        day = d.timetuple().tm_yday                   # gets the day of the year aprox
         from_time = 2*math.cos(2*day*math.pi/365)+19 # will range from about 5 - 9pm
         to_time = 9                                  # not too fussed what the end time is.
         h = d.hour + d.minute / 60                   # gets the hour of day with decimal
-        b = int(255 - 254 * from_time < h | h < to_time)
-        time.sleep(60)
+        b = int(255 - 254 * (from_time < h or h < to_time))
+        for i in range(60):
+            if stop:
+                return
+            time.sleep(1)
 
 # TODO slot in updating b -- only need to do it once per minute buttt hmmmmm or even less.....
 # that should be okay now...
 
-brightness = threading.Thread(target=updateB)
-brightness.start()
 
 while 1:
     try:
+        brightness = threading.Thread(target=updateB) # creates thread that should be stopped on keyboard interrupt
+        brightness.start()
         while 1:
             c = coin.getNextPrice()
+            print(b)
             if not first:
                 change += c
             else:
                 first = False
-            upd = threading.Thread(target=update, args=(strip,c,prev,colors,d))
+            upd = threading.Thread(target=update, args=(strip,c,prev,b,d))
             upd.start()
             prev = c
 
     except KeyboardInterrupt: # this handles process kill requests aswell.
+        stop = True
         quit()
-        break
+        exit()
     except requests.exceptions.ConnectionError:
         coin.log(f"Some error connecting occurred. http status: {coin.s}")
         time.sleep(1)
